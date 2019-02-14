3152ffc431c92be3ba9b801884b9c00ba7f30b61
Manas Mishra <manas1.mishra@ril.com>
2019-02-14 23:46:47 +0530

    Initial Commit

---
+++ CustomSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomSlider
+//
+//  Created by Manas Mishra on 14/02/19.
+//  Copyright © 2019 manas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
